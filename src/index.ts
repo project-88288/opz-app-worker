339a9ffa40e39d203af22c7c8f4bce860ff3f80d
project-88288 <chaiya.88288@gmail.com>
2023-06-04 12:46:04 +0700

    update

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,6 @@
 require('dotenv').config();
 import { initServer } from './loders/server';
 import { collect } from './collector/collect';
-import * as logger from './lib/logger'
-import * as bluebird from 'bluebird'
-
-bluebird.Promise.config({ longStackTraces: true, warnings: { wForgottenReturn: false } })
-global.Promise = bluebird as any // eslint-disable-line
-
-
 
 if (require.main === module) {
     initServer()
