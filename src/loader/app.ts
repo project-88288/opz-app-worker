2af7b6840a07f520c1a4fc04513d7041928e0d76
project-88288 <chaiya.88288@gmail.com>
2023-06-09 01:23:24 +0700

    update

--- src/loader/app.ts
+++ src/loader/app.ts
@@ -2,35 +2,45 @@
 
 require('dotenv').config();
 import Koa from 'koa';
-import Router from 'koa-router';
-import bodyParser from 'koa-bodyparser';
+const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser')
 import { UserController } from '../controllers/userController';
 import * as logger from '../lib/logger'
 import * as http from 'http'
+import { exec } from 'child_process';
 
 const API_VERSION_PREFIX = '/v1'
 const CORS_REGEXP = /^https:\/\/(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.){0,3}terra\.dev(?::\d{4,5})?(?:\/|$)/
 let isShuttingDown = false;
 let connections: http.ServerResponse[] = [];
-
-export async function initApp(): Promise<Koa> {
+const { DATABASE_URL, AZ_CONTAINER } = process.env
+let server: http.Server
+export async function initApp(): Promise<http.Server> {
   const app = new Koa();
-  app.use((ctx, next) => {
-    if (isShuttingDown) {
-      ctx.status = 503; 
-      ctx.body = 'Server is shutting down';
-    } else {
-      next();
-    }
+  const router = Router();
+
+  // Middleware
+ app.use(bodyParser());
+  // Routes
+  router.get('/', (ctx) => {
+    ctx.body = 'Hello, Koa!';
   });
-  app.use(async (ctx, next) => {
-    connections.push(ctx.res);
-    await next();
-    connections = connections.filter((res) => res !== ctx.res);
-  });
-  return app
+
+  app.use(router.routes()).use(router.allowedMethods());
+  
+  // Start the server
+  server = http.createServer(app.callback())
+  const port = DATABASE_URL ? 3100 : 3101
+  server.listen(port, () => {
+    logger.warn(`Listening on port ${port}`)
+    exec(`xdg-open http://localhost:${port}`);
+  })
+  return server
 }
 
+export function getServer() {
+  return server
+}
 
 export function stopAcceptingConnections() {
   if (!isShuttingDown) isShuttingDown = true
--- src/loader/server.ts
+++ src/loader/server.ts
@@ -4,7 +4,7 @@ require('dotenv').config();
 import * as bluebird from 'bluebird'
 import * as http from 'http'
 import * as logger from 'lib/logger'
-import { initApp } from './app';
+import { getServer, initApp } from './app';
 import { gracefulShutdown } from 'lib/shutdown';
 import { finalizeORM, initORM } from 'orm/connection';
 import { cronJobStart, downloadBlockHeight, downloadJson, updateLatestHeight, updateipv6, } from 'lib/cronjob';
@@ -13,9 +13,9 @@ bluebird.Promise.config({ longStackTraces: true, warnings: { wForgottenReturn: f
 global.Promise = bluebird as any // eslint-disable-line
 
 let server: http.Server
-
+const { DATABASE_URL,AZ_CONTAINER } = process.env
 export async function initServer(): Promise<http.Server> {
-  const { DATABASE_URL,AZ_CONTAINER } = process.env
+ 
   logger.info('Initialize app')
   const app = await initApp()
 
@@ -23,11 +23,7 @@ export async function initServer(): Promise<http.Server> {
   await initORM()
   bluebird.Promise.delay(2000)
   logger.log(`Azure container using "${AZ_CONTAINER}"`)
-  server = http.createServer(app.callback())
-  const port = DATABASE_URL ? 3100 : 3101
-  server.listen(port, () => {
-    logger.warn(`Listening on port ${port}`)
-  })
+
   bluebird.Promise.delay(2000)
   logger.info('Download from cloud')
   await downloadJson()
@@ -43,10 +39,7 @@ export async function initServer(): Promise<http.Server> {
   process.on('SIGTERM', gracefulShutdown);
   process.on('SIGINT', gracefulShutdown);
   process.on('SIGHUP', gracefulShutdown);
-  return server
-}
-
-export function getServer() {
+  server = getServer()
   return server
 }
 
