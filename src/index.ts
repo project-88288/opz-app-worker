7f350fb047129c6a1346c4597e2a9113eb7c6d01
project-88288 <chaiya.88288@gmail.com>
2023-06-09 03:06:27 +0700

    update

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,6 @@ require('dotenv').config();
 import * as logger from 'lib/logger'
 import { initServer } from './loader/server';
 import { collect } from './collector/collect';
-import { block_pull, block_push, caches_pull, cachses_push } from 'collector/caches';
 
 if(process.env.DATABASE_URL) {
     logger.warn(`DATABASE_URL:${process.env.DATABASE_URL}`)
@@ -12,10 +11,5 @@ if(process.env.DATABASE_URL) {
 
 if (require.main === module) {
   initServer()
- // collect()
+  collect()
 }
-
-//caches_pull('names')
-//cachses_push('test')
-//block_push('test',['block.json'])
-//block_pull('test',['block.json'])
